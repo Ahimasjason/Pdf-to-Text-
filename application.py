562da2e78267cef73321e6865483ff272178766b
Ahimas <ahimas.jasan@gmail.com>
2020-02-01 19:22:34 +0530

    option to download and view'

--- application.py
+++ application.py
@@ -33,7 +33,17 @@ def convert_pdf(file):
 
 
 '''
-from flask import Flask, send_file, render_template, render_template_string, request, current_app
+from flask import(
+    Flask,
+    send_file,
+    render_template,
+    render_template_string,
+    request,
+    current_app,
+    url_for,
+    send_from_directory,
+    redirect,
+)
 from wtforms import Form , FileField, SubmitField
 import os
 
@@ -43,6 +53,16 @@ class UploadForm(Form):
 
 
 TEMPLATE_STRING = '''
+{% for i in files %}
+<ul>
+<li> {{ i }}
+<a href='{{ url_for('download_file', file_name= i,action='view') }}'> View</a>
+<a href='{{ url_for('download_file', file_name= i) }}' >Download </a>
+</li>
+</ul>
+{% endfor  %}
+
+
 <form method="post" enctype="multipart/form-data">
 <div class='row'>
 
@@ -58,29 +78,62 @@ TEMPLATE_STRING = '''
 print(__name__)
 app = Flask(__name__)
 
+TEXTFOLDER = 'text_files'
+
 def write_file(text, file_name):
     static_folder = current_app.static_folder
 
-    with open(os.path.join(static_folder,'text_files',file_name + '.txt'),'w') as f:
+    with open(os.path.join(static_folder,TEXTFOLDER,file_name + '.txt'),'w') as f:
         f.write(text)
+
+def get_all_files():
+    static_folder = current_app.static_folder
+    path =os.path.join(static_folder,TEXTFOLDER)
+    if not os.path.isdir(path):
+        return ''
+    return os.listdir(path)
 
 @app.route('/',methods=['GET','POST'])
 def index():
     form = UploadForm()
     if  request.method == 'POST':
-        print(current_app.static_folder)
         file = request.files['upload_file']
-        text = convert_pdf(file )
-        print(file )
+        if not file:
+            print (" No file part found")
+            return redirect(request.url)
 
-        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_extension = file.filename.split('.')[-1]
+        if file_extension != 'pdf':
+            return redirect(request.url)
+        text = convert_pdf(file )
+        #
+        # dir_path = os.path.dirname(os.path.realpath(__file__))
         static_path = current_app.static_folder
-        if not os.path.isdir(os.path.join(static_path, 'text_files')):
-            os.mkdir(os.path.join(static_path,'text_files'))
+        if not os.path.isdir(os.path.join(static_path, TEXTFOLDER)):
+            os.mkdir(os.path.join(static_path,TEXTFOLDER))
         expected_filename = file.filename.split('.')[0]+'.txt'
         write_file(text, expected_filename)
-        print(" At POST Method")
-    return render_template_string(TEMPLATE_STRING , form=form)
+    all_txt_file = get_all_files()
+    return render_template_string(TEMPLATE_STRING , form=form, files=all_txt_file)
+
+@app.route('/download_file')
+def download_file():
+    file_name = request.args.get("file_name", None)
+    action = request.args.get('action', None)
+
+    if file_name is None :
+        raise ValueError(" No file ref provided")
+    static_path = current_app.static_folder
+    folder = os.path.join(static_path, TEXTFOLDER)
+    if not os.path.isdir(folder):
+        raise FileNotFoundError(" No folder exist ")
+    list_of_files = os.listdir(folder)
+    if file_name not in list_of_files :
+        raise FileNotFoundError("  Unable to find file ")
+    if action == 'view':
+        return send_file(os.path.join(folder,file_name))
+    return send_file(os.path.join(folder,file_name), as_attachment= True)
+    # return send_from_directory(current_app.static_folder,TEXTFOLDER+file_name)
 
 if __name__ == '__main__':
     app.secret_key = "secret"
